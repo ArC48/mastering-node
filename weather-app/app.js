da9767900646d6cc9b07a90b8720ba2be2d56d07
ArC48 <beqa3352@gmail.com>
2024-08-20 01:28:24 +0400

    started asynchronous

---
+++ weather-app/app.js
@@ -0,0 +1,14 @@
+console.log("Starting");
+
+setTimeout(() => {
+    console.log("2");
+}, 2000);
+
+setTimeout(() => {
+    console.log("0");
+}, 0);
+
+// for (let i = 0; i < 1000000; i++) {
+//     console.log(i);
+// }
+console.log("Stopping");
