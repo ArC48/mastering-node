a6517cd4cc8dfe9d83efe87a803b23944ca29c3a
ArC48 <beqa3352@gmail.com>
2024-08-21 11:44:09 +0400

    object destructuring

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,30 @@
+const name = "Beqa";
+const userAge = 23;
+
+const user = {
+    name,
+    age: userAge,
+    location: "Tbilisi",
+};
+
+console.log(user);
+
+//  Object destructuring
+
+const product = {
+    label: "blue keyboard",
+    price: 100,
+    stock: 345,
+    salePrice: undefined,
+};
+
+// const { label: itemLabel, price, rating = "N/A" } = product;
+
+// console.log(itemLabel, price, rating);
+
+// const transaction = (type, { label, stock }) => {
+//     console.log(label);
+//     console.log(stock);
+// };
+
+// transaction("order", product);
