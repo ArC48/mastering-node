a62719a0ee4fbead784884e00d0d2fd2af0b6a21
ArC48 <beqa3352@gmail.com>
2024-08-22 12:44:23 +0400

    hbs intro

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,27 @@ const express = require("express");
 const app = express();
 const publicDirectory = path.join(__dirname, "..\\public");
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(publicDirectory));
+
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather Corner",
+        name: "Archmage Ged",
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Get Help!",
+        buttonText: "Send Feedback",
+    });
+});
 
 app.get("/weather", (req, res) => {
     res.send([
