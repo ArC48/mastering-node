65666c66e98dffa9d1c95b22e10eab4dc3cca801
ArC48 <beqa3352@gmail.com>
2024-08-26 15:43:01 +0400

    dynamic data added to the UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,34 @@
-console.log("Client side js file is loaded!");
+const weatherForm = document.querySelector("form");
+const searchElement = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+const weatherPic = document.querySelector("#weather-pic");
+
+const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+// messageOne.textContent = "dsa";
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    messageOne.textContent = `Loading...`;
+    messageTwo.textContent = "";
+    weatherPic.src = "";
+
+    fetch(`http://localhost:3000/weather?address=${searchElement.value}`).then(
+        (response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error;
+                } else {
+                    weatherPic.src = data.forecast.weather_icons[0];
+                    messageOne.textContent = `It's ${
+                        data.forecast.weather_descriptions[0]
+                    } in ${capitalize(data.location)}`;
+                    messageTwo.textContent = `${data.forecast.temperature}°C`;
+                }
+            });
+        }
+    );
+});
--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -8,7 +8,7 @@ const geocode = (address, callback) => {
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to connect to location services", undefined);
-        } else if (body.data.length === 0) {
+        } else if (!body.data || body.data.length === 0) {
             callback("Unable to find location, Try another one!", undefined);
         } else {
             const { latitude, longitude, name: location } = body.data[0];
