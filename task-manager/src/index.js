cb7be35d5872432f7e310053070a540bd3fb6f64
ArC48 <beqa3352@gmail.com>
2024-09-05 16:42:00 +0400

    added comments to index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,21 +1,28 @@
-// initialize express server
+// (initialize express server)
 
 const express = require("express");
+
+// Establish connection to the database (mongoose handles the connection)
 require("./db/mongoose");
 
 // models
 const User = require("./models/user");
 const Task = require("./models/task");
 
+// Create an Express application instance
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware to parse incoming JSON requests into JavaScript objects
 app.use(express.json());
 
+// POST request handler for creating new users
 app.post("/users", (req, res) => {
+    // Create a new user using the data from the request body
     const user = new User(req.body);
     user.save()
         .then(() => {
+            // we can chain result methods and send custom status
             res.status(201).send(`${user.name} added!`);
         })
         .catch((e) => {
@@ -23,6 +30,7 @@ app.post("/users", (req, res) => {
         });
 });
 
+// POST request handler for creating new tasks
 app.post("/tasks", (req, res) => {
     const task = new Task(req.body);
     task.save()
@@ -34,6 +42,7 @@ app.post("/tasks", (req, res) => {
         });
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
     console.log("Listening on Port: " + port);
 });
