caeb27fdd616d32bd6711f0257053ab7979add2c
ArC48 <beqa3352@gmail.com>
2024-09-17 16:46:30 +0400

    Integrated async/await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -17,79 +17,71 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // POST request handler for creating new users
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
     // Create a new user using the data from the request body
     const user = new User(req.body);
-    user.save()
-        .then(() => {
-            // we can chain result methods and send custom status
-            res.status(201).send(`${user.name} added!`);
-        })
-        .catch((e) => {
-            res.status(400).send(e);
-        });
+    try {
+        await user.save();
+        res.status(201).send(`${user.name} added!`);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
-app.get("/users", (req, res) => {
-    User.find({})
-        .then((users) => {
-            res.send(users);
-        })
-        .catch((e) => {
-            res.status(500).send(e);
-        });
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
+    _id = req.params.id;
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send({ error: "User Not Found!" });
+        }
+        res.send(user);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+app.get("/tasks/:id", async (req, res) => {
     _id = req.params.id;
 
-    User.findById(_id)
-        .then((user) => {
-            if (!user) {
-                return res.status(404).send({ error: "User Not Found!" });
-            }
-            res.send(user);
-        })
-        .catch((e) => {
-            res.status(500).send(e);
-        });
-});
-
-app.get("/tasks", (req, res) => {
-    Task.find({})
-        .then((tasks) => {
-            res.send(tasks);
-        })
-        .catch((e) => {
-            res.status(500).send(e);
-        });
-});
-
-app.get("/tasks/:id", (req, res) => {
-    _id = req.params.id;
-
-    Task.findById(_id)
-        .then((task) => {
-            if (!task) {
-                return res.status(404).send({ error: "task not found!" });
-            }
-            res.send(task);
-        })
-        .catch((e) => {
-            res.status(500).send(e);
-        });
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send({ error: "task not found!" });
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 // POST request handler for creating new tasks
-app.post("/tasks", (req, res) => {
-    const task = new Task(req.body);
-    task.save()
-        .then(() => {
-            res.status(201).send(`Task {${task.title}} added!`);
-        })
-        .catch((e) => {
-            res.status(400).send(e);
-        });
+app.post("/tasks", async (req, res) => {
+    try {
+        const task = new Task(req.body);
+        await task.save();
+        res.status(201).send(`Task {${task.title}} added!`);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 // Start the server and listen on the specified port
