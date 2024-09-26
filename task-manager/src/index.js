2c842219cb135ef509f462e3be6711d2bb081c76
ArC48 <beqa3352@gmail.com>
2024-09-26 11:54:04 +0400

    refactored User routes

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,9 +5,9 @@ const express = require("express");
 // Establish connection to the database (mongoose handles the connection)
 require("./db/mongoose");
 
-// models
-const User = require("./models/user");
 const Task = require("./models/task");
+
+const userRouter = require("./routers/user");
 
 // Create an Express application instance
 const app = express();
@@ -15,80 +15,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware to parse incoming JSON requests into JavaScript objects
 app.use(express.json());
-
-app.get("/users", async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-app.get("/users/:id", async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send({ error: "User Not Found!" });
-        }
-        res.send(user);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-app.post("/users", async (req, res) => {
-    // Create a new user using the data from the request body
-    const user = new User(req.body);
-    try {
-        await user.save();
-        res.status(201).send(`${user.name} added!`);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-});
-
-app.patch("/users/:id", async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ["name", "email", "password", "age"];
-    const isValidOperation = updates.every((update) => {
-        return allowedUpdates.includes(update);
-    });
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: "Invalid keys to change!" });
-    }
-
-    try {
-        const _id = req.params.id;
-        const user = await User.findByIdAndUpdate(_id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-});
-
-app.delete("/users/:id", async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const deletedUser = await User.findByIdAndDelete(_id);
-        if (!deletedUser) {
-            return res.status(404).send({ Error: "User not found" });
-        }
-        res.send(deletedUser);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
+app.use(userRouter);
 
 app.get("/tasks", async (req, res) => {
     try {
